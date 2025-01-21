ee12ab9f82d18502552baa7e08ccdaf7cf97b76c
Ricky Mok <cskpmok@caida.org>
2025-01-21 12:53:22 -0800

    update bootstrap script

--- slurm_scripts/bootstrap_env.sh
+++ slurm_scripts/bootstrap_env.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/bash
+SLURM_JOB_ID=squeue -u $LOGNAME |grep $HOSTNAME | awk '{print $1}'
+
 cd "/scratch/${USER}/job_${SLURM_JOB_ID}"
-cp ~/spark_env/* "/scratch/${USER}/job_${SLURM_JOB_ID}/"
+cp -r ~/telescope_expanse_tutorial "/scratch/${USER}/job_${SLURM_JOB_ID}/"
 python3.9 -m venv .venv
 . ./.venv/bin/activate
 pip install --upgrade pip
-pip install jupyter pyspark==3.2.1 pandas matplotlib seaborn scipy
+pip install jupyter pyspark==3.2.1 pandas matplotlib seaborn scipy findspark
 
 # 1 day of flowtuples
 # compute-1x128_128G.slurm   3min 51s
