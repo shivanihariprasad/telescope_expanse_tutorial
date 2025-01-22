cbd566538749b812031d3291db808e54eddeeb84
Ricky Mok <cskpmok@caida.org>
2025-01-22 13:12:59 -0800

    fix run nb

--- example_notebooks/run_notebook.sh
+++ example_notebooks/run_notebook.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-SLURM_JOB_ID=squeue -u $LOGNAME |grep $HOSTNAME | awk '{print $1}'
+SLURM_JOB_ID="$(squeue -u $LOGNAME |grep $HOSTNAME | awk '{print $1}')"
 cd "/scratch/${USER}/job_${SLURM_JOB_ID}"; 
 jupyter-lab --port 9000 --no-browser
