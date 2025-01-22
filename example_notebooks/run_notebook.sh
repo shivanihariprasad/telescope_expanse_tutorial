8bafd013fedb7f6840d2d26fdddbf618573f8771
Ricky Mok <cskpmok@caida.org>
2025-01-22 13:14:30 -0800

    fix run nb

--- example_notebooks/run_notebook.sh
+++ example_notebooks/run_notebook.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 SLURM_JOB_ID="$(squeue -u $LOGNAME |grep $HOSTNAME | awk '{print $1}')"
 cd "/scratch/${USER}/job_${SLURM_JOB_ID}"; 
+python3.9 -m venv .venv;
+. ./.venv/bin/activate;
 jupyter-lab --port 9000 --no-browser
